83ba9817008792fbf2237e835364d181007f66c3
Dustin <drschwenk@gmail.com>
2024-06-05 12:04:42 -0700

    config for llamaish7 baseline with normal init

--- scripts/beaker/llamaish7-launch.sh
+++ scripts/beaker/llamaish7-launch.sh
@@ -5,12 +5,13 @@ set -ex
 NUM_NODES=8
 
 gantry run \
-  --workspace ai2/dirkg \
-  --task-name llamaish7 \
-  --description "OLMo medium - 7B - Llamaish" \
+  --workspace ai2/OLMo-training  \
+  --task-name llamaish7-dirk-baseline-normal-init \
+  --description "OLMO 7B with congif matching Dirks baseline and normal init" \
   --priority high \
+  --preemptible \
   --beaker-image shanea/olmo-torch2.2-gantry \
-  --cluster ai2/pluto-cirrascale \
+  --cluster ai2/jupiter-cirrascale \
   --gpus 8 \
   --replicas "${NUM_NODES}" \
   --leader-selection \
@@ -18,15 +19,15 @@ gantry run \
   --budget ai2/oe-training \
   --no-nfs \
   --propagate-failure \
-  --synchronized-start-timeout 10m \
+  --synchronized-start-timeout 20m \
   --env LOG_FILTER_TYPE=local_rank0_only \
   --env OMP_NUM_THREADS=8 \
   --env OLMO_TASK=model \
-  --env-secret WANDB_API_KEY=WANDB_API_KEY \
-  --env-secret AWS_ACCESS_KEY_ID=AWS_ACCESS_KEY_ID \
-  --env-secret AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY \
+  --env-secret WANDB_API_KEY=DUSTINS_WANDB_API_KEY \
+  --env-secret AWS_ACCESS_KEY_ID=DUSTINS_AWS_ACCESS_KEY_ID \
+  --env-secret AWS_SECRET_ACCESS_KEY=DUSTINS_AWS_SECRET_ACCESS_KEY \
   --shared-memory 10GiB \
   --venv base \
   --yes \
   --timeout=-1 \
-  -- /bin/bash -c "scripts/beaker/llamaish7.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
+  -- /bin/bash -c "scripts/beaker/llm-360-amber.sh \$BEAKER_LEADER_REPLICA_HOSTNAME ${NUM_NODES} \$BEAKER_REPLICA_RANK"
--- scripts/beaker/llamaish7.sh
+++ scripts/beaker/llamaish7.sh
@@ -11,13 +11,6 @@ shift
 BEAKER_REPLICA_RANK=$1
 shift
 
-# Warm HF cache
-mkdir -p /root/.cache
-pushd /root/.cache
-curl "https://storage.googleapis.com/dirkgr-public/huggingface_cache_v3.tar.gz" | tar --keep-newer-files -xzf -
-popd
-export HF_DATASETS_OFFLINE=1
-
 torchrun \
   --nnodes ${NUM_NODES}:${NUM_NODES} \
   --nproc-per-node 8 \
@@ -27,20 +20,8 @@ torchrun \
   --node_rank=$BEAKER_REPLICA_RANK \
   --rdzv_conf="read_timeout=420" \
   scripts/train.py \
-  configs/llamaish7-s3.yaml \
-    --run_name=llamaish7-detailed \
-    --wandb.name=llamaish7-detailed \
-    --wandb.group=llamaish7-detailed \
-    --model.flash_attention=true \
-    --fsdp.wrapping_strategy=by_block_and_size \
-    --fsdp.sharding_strategy=SHARD_GRAD_OP \
+  configs/llamaish7-normal-s3.yaml \
     --save_folder=runs/ \
     --activation_checkpointing=fine_grained \
     --fused_loss=true \
-    --device_train_microbatch_size=2 \
-    --global_train_batch_size=1024 \
-    --save_interval=50 \
-    --eval_interval=50 \
-    --optimizer.metrics_log_interval=1 \
-    --save_overwrite \
-    '--load_path=${path.last_checkpoint:s3://ai2-llm/checkpoints/OLMo-medium/llamaish7-detailed/}'
+    --save-overwrite
